5017f25b512416cf9fb210b63418ab0a093df872
CserTamasJanos <70898590+CserTamasJanos@users.noreply.github.com>
2022-05-16 21:30:38 +0200

    RoboCop v0.0.0.0.1

---
+++ RoboCop5/Controllers/CaseRiportController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoboCop5.Models;
+
+namespace RoboCop5.Controllers
+{
+    public class CaseRiportController : Controller
+    {
+        private RoboCopEntities db = new RoboCopEntities();
+        // GET: CaseRiport
+        public ActionResult Index()
+        {
+            //CaseRiport riport = new CaseRiport();
+
+            //riport.County = db.Megyek.Select(m => m.MNAME).ToList();
+            //riport.City = db.Varosok.Select(v => v.VNAME).ToList();
+            //riport.PublicSpaceType = db.KozteruletTipusok.Select(k => k.KNAME).ToList();
+            //riport.FaceTypes = db.Arcok.Select(a => a.ARNAME).ToList();
+            //riport.HairColors = db.Hajszinek.Select(h => h.HANAME).ToList();
+            //riport.WalkingTypes = db.Jarasok.Select(j => j.JANAME).ToList();
+            //riport.Heights = db.Magassagok.Select(m=> m.MAINTERVAL).ToList();
+            //riport.Weights = db.Sulyok.Select(s => s.SUINTERVAL).ToList();
+            //riport.EyeColors = db.Szemszinek.Select(sz => sz.SZENAME).ToList();
+            //riport.BodyTypes = db.Termetek.Select(t => t.TENAME).ToList();
+            //riport.Tools = db.Targyak.Select(tar => tar.TNAME).ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Riport(string riport)
+        {
+            return View();
+        }
+
+        public ActionResult Riport()
+        {
+            CaseRiport riport = new CaseRiport();
+
+            riport.County = db.Megyek.Select(m => m.MNAME).ToList();
+            riport.City = db.Varosok.Select(v => v.VNAME).ToList();
+            riport.PublicSpaceType = db.KozteruletTipusok.Select(k => k.KNAME).ToList();
+            riport.FaceTypes = db.Arcok.Select(a => a.ARNAME).ToList();
+            riport.HairColors = db.Hajszinek.Select(h => h.HANAME).ToList();
+            riport.WalkingTypes = db.Jarasok.Select(j => j.JANAME).ToList();
+            riport.Heights = db.Magassagok.Select(m => m.MAINTERVAL).ToList();
+            riport.Weights = db.Sulyok.Select(s => s.SUINTERVAL).ToList();
+            riport.EyeColors = db.Szemszinek.Select(sz => sz.SZENAME).ToList();
+            riport.BodyTypes = db.Termetek.Select(t => t.TENAME).ToList();
+            riport.Tools = db.Targyak.Select(tar => tar.TNAME).ToList();
+
+            //Navbar n = new Navbar();
+            //HttpCookie nameCookie = Request.Cookies["RcCookie"];
+            //string name = nameCookie != null ? nameCookie.Value.Split('=')[0] : "undefined";
+            //n.UsersMenus = db.UsersMenus(name).ToList();
+            //ViewData["Menu"] = n;
+            return View(riport);
+        }
+
+        public ActionResult SaveRiport(CaseRiport saveRiport)
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ RoboCop5/Controllers/NavController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoboCop5.Models;
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace RoboCop5.Controllers
+{
+    public class NavController : Controller
+    {
+        private RoboCopEntities db = new RoboCopEntities();
+        private string secretStringForToken = "AVeryBigSecretKey";
+        // GET: Nav
+        public ActionResult Navbar()
+        {
+            Navbar n = new Navbar();
+            //var cookie = HttpContext.Request.Cookies.Get("token").Value;
+            //HttpCookie cookie =  ;
+
+
+            string passCookie = TempData["passCookie"] as string;
+            HttpCookie nameCookie = Request.Cookies[passCookie];
+
+            //If Cookie exists fetch its value.
+            string name = nameCookie != null ? nameCookie.Value.Split('=')[0] : "undefined";
+
+            //TempData["Message"] = name;
+
+            //return RedirectToAction("Index");
+
+            n.UsersMenus = db.UsersMenus(name).ToList();
+            return PartialView(n);
+            //RoboCopEntities r = new RoboCopEntities();
+            //Navbar n = new Navbar();
+
+            //token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIyNiIsIm5iZiI6MTY1MTU5MTg3MCwiZXhwIjoxNjUxNTkxOTMwLCJpYXQiOjE2NTE1OTE4NzB9.z2ixPi97FYmhR4w_eelmQMOLLUDcdEOBT4ITfEnasQs";
+            //n.UsersMenus = r.UsersMenus(token).ToList();
+            //HttpContext.Request.Cookies.Get(token);
+            //return RedirectToAction("Riport", "CaseRiport");
+            //ViewData["Menu"] = n;
+        }
+
+        public int? ValidateToken(string token)
+        {
+            if (token == null)
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretStringForToken);
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+
+                // return user id from JWT token if validation successful
+                return userId;
+            }
+            catch
+            {
+                // return null if validation fails
+                return null;
+            }
+        }
+
+
+    }
+}
